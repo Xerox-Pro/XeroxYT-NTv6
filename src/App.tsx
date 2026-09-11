daea07082d5fffb7d951e533b50e534026b6cb49
soranous <oz.ozk.takeru0000@gmail.com>
2026-09-11 16:16:04 +0900

    refactor: remove privacy screen feature
    Removed the PrivacyOverlay component and its associated toggle in the Navbar to simplify the UI.

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,10 @@ import { Routes, Route } from 'react-router-dom';
 import MainApp from './MainApp';
 import AIStudio from './components/AIStudio';
 import PWAUpdateIndicator from './components/PWAUpdateIndicator';
-import PrivacyOverlay from './components/PrivacyOverlay';
 
 export default function App() {
   return (
     <>
-      <PrivacyOverlay />
       <PWAUpdateIndicator />
       <Routes>
         <Route path="/*" element={<MainApp />} />
