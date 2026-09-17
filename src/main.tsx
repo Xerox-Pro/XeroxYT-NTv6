d1195887371acf4a77e8e47ed52f3f3f0a4ebf33
soranous <oz.ozk.takeru0000@gmail.com>
2026-09-17 10:14:14 +0900

    refactor: remove liquidglass dependency and theme logic
    Clean up project structure by removing the unused liquidglass library, the associated ThemeContext, and refactoring the App component for standard routing.

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,6 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App.tsx';
 import './index.css';
 import { initPWA } from './pwa';
-import { ThemeProvider } from './ThemeContext';
 
 // サイト変更検知＆自動アップデートの初期化
 initPWA();
@@ -12,9 +11,7 @@ initPWA();
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
-      <ThemeProvider>
-        <App />
-      </ThemeProvider>
+      <App />
     </BrowserRouter>
   </StrictMode>,
 );
